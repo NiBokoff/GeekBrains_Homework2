6812ebe73b6db525bbc73d5e939aea558f1b90a8
NiBo <RusMDK@gmail.com>
2022-08-23 22:26:53 +0300

    created task_2 hw

---
+++ task_2.cs
@@ -0,0 +1,10 @@
+// Задача 2: Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) или сообщает, что третьей цифры нет.
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+    Console.WriteLine("-> третьей цифры нет");
+}
